5b1805c7a6b4cd7c214c8636650acf08f904bb06
emanjaber <jaber0eman@gmail.com>
2021-04-15 18:59:20 +0300

    edit js code

--- js/countryMealsScript.js
+++ js/countryMealsScript.js
@@ -10,41 +10,57 @@ function viweArea(){
    area.appendChild(areaN);
    header.appendChild(area);
 
-   
-
 }
+
 function viewMeals(){
     let meals2 = document.getElementById("meals");
 
     
-
-
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${areaName2}`)
     .then(response => {
         return response.json();      
     })    
     .then(data => {
-        data.array.forEach(meals => {
-            
+
+        for (let index = 0; index < 4; index++) {
+            let meal = meals[index];
+
             let html = "";
             html += `
             <a href="./meal.html">
                 <div class = "meal">
-                <img src="${meals.strMealThumb}" alt="">
-                <h3>${meals.strMeal}</h3>
-                <h4>${meals.strCategory}</h4>        
+                <img src="${meal.strMealThumb}" alt="meal">
+                <h3>${meal.strMeal}</h3>
+                <h4>${meal.strCategory}</h4>        
                 </div>
             </a>
             `
             meals2.innerHTML += html;
+
+            
+        }
+
+        // meals.forEach(meal => {
+            
+        //     let html = "";
+        //     html += `
+        //     <a href="./meal.html">
+        //         <div class = "meal">
+        //         <img src="${meals.strMealThumb}" alt="">
+        //         <h3>${meals.strMeal}</h3>
+        //         <h4>${meals.strCategory}</h4>        
+        //         </div>
+        //     </a>
+        //     `
+        //     meals2.innerHTML += html;
         
-        // mealImage.src = data.strMealThumb;
-        // mealName.textContent = data.strMeal;
-        // mealCategory.textContent = data.strCategory;
+        // // mealImage.src = data.strMealThumb;
+        // // mealName.textContent = data.strMeal;
+        // // mealCategory.textContent = data.strCategory;
 
 
 
-        });
+        // });
         
 
     })
@@ -53,3 +69,9 @@ function viewMeals(){
 
 
 }
+
+
+window.onload = (event) => {
+    viweArea();
+    viewMeals();
+};
